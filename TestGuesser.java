b1f605b1f410ae5745f446c4f2315a359f218965
pontus <bollpiska@hotmail.com>
2019-10-24 11:44:02 +0200

    skapar klassen TestGuesser för att testa att konstruktorn fungerar

---
+++ TestGuesser.java
@@ -0,0 +1,10 @@
+class TestGuesser {
+  public static void main (String[] args){
+    Guesser gissare = new Guesser(3, 100);
+    Guesser gissare2 = new Guesser(20, 4);
+    Guesser gissare3 = new Guesser(3, 3);
+
+  }
+
+
+}
