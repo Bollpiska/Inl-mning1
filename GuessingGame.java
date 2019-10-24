740cc43f8dbbd6238b29fa5b737c8dbb99b3d02c
pontus <bollpiska@hotmail.com>
2019-10-24 13:54:23 +0200

    added user input to GuessingGame

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,6 +1,20 @@
 class GuessingGame{
   public static void main (String[] args){
-    Guesser gissare = new Guesser(0, 100);
+    int low = 0;
+    int high = 0;
+    try{
+      low = Integer.parseInt(args[0]);
+  }
+    catch(NumberFormatException e){
+      System.out.println("Bad input. Both arguments must be integers");
+    }
+    try{
+      high = Integer.parseInt(args[1]);
+  }
+    catch(NumberFormatException e){
+      System.out.println("Bad input. Both arguments must be integers");
+    }
+    Guesser gissare = new Guesser(low, high);
     gissare.start();
 
   }
