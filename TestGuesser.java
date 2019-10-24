c1bc8c85c364ab4723227d7889c9b5bf9cd80e4e
pontus <bollpiska@hotmail.com>
2019-10-24 11:52:29 +0200

    uppdaterar TestGuesser för att använda toString-metoden

--- TestGuesser.java
+++ TestGuesser.java
@@ -1,6 +1,7 @@
 class TestGuesser {
   public static void main (String[] args){
     Guesser gissare = new Guesser(3, 100);
+    System.out.println(gissare.toString());
     Guesser gissare2 = new Guesser(20, 4);
     Guesser gissare3 = new Guesser(3, 3);
 
